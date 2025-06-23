24038aaca866f1c855360bd5bca087b79ec3d0ac
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-06-23 22:59:18 +0530

    Create Person_Fetch.java

---
+++ First_Hibernate_Project/src/main/java/com/org/fetchingData/Person_Fetch.java
@@ -0,0 +1,55 @@
+package com.org.fetchingData;
+
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
+
+public class Person_Fetch {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Session session= new Configuration().configure().buildSessionFactory().openSession();
+		
+		
+		Person p1 = session.get(Person.class, 101);
+		
+		System.out.println(p1);
+		
+		 //*****Similarity between get() and load()
+        // Person p1=session.get(Person.class, 112);
+         //System.out.println(p1);
+         
+         
+         //Person p1=session.load(Person.class, 112);
+        // System.out.println(p1);
+         
+         
+         
+         //*****1.difference between get() and load()
+        // Person p1=session.get(Person.class, 120);       //get() returns null if the given id is not available in the database
+        //System.out.println(p1);
+         
+        // Person p1=session.load(Person.class, 120);      //load() returns ObjectNotFoundException if the given id is not available in the database
+         //System.out.println(p1);
+         
+         
+         
+        //*****2.difference between get() and load()
+        // Person p1=session.get(Person.class, 112);//get() will  fire select query everytime whenever we are using that object or not
+
+        //System.out.println(p1.getEid());    //get() will  fire select query
+        //System.out.println(p2.getName());  //get() will  fire select query
+        //System.out.println(p2);     //get() will fire select query when we access whole object
+             
+         
+         
+         Person p2=session.load(Person.class, 112);   //load() will NOT fire select query if we are not using/printing that whole object or its one of the property except id
+            //System.out.println(p2.getEid());    //load() will NOT fire select query(lazy loading)
+           //System.out.println(p2.getName());        //load() will fire select query(lazy loading) when we are getting properties of object(other than id)
+                System.out.println(p2);                    //load() will fire query when we access whole object
+        
+		
+	}
+
+}
