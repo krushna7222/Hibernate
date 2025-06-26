7bd11bbb78de68612ce9d6de79aabff2f9b8a2d7
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-06-26 21:31:56 +0530

    Create Employee.java

---
+++ One_To_Many_Mapping/src/main/java/com/org/Employee.java
@@ -0,0 +1,28 @@
+package com.org;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int emp_id;
+	private String name;
+	
+	@ManyToOne
+	@JoinColumn(name="dept_id")
+	private Department Department;
+	
+}
