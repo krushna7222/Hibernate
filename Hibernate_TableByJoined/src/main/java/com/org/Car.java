736b8c84b2342555ca3cc432653ff5d88a7011c3
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-06-29 22:19:42 +0530

    Create Car.java

---
+++ Hibernate_TableByJoined/src/main/java/com/org/Car.java
@@ -0,0 +1,16 @@
+package com.org;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Car extends Vehicle{
+	
+	private int speed;
+
+}
