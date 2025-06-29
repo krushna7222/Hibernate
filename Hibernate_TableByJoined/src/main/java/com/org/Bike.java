86d5dc21f72344ac7578e8491c9ffa8d3e18f033
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-06-29 22:19:47 +0530

    Create Bike.java

---
+++ Hibernate_TableByJoined/src/main/java/com/org/Bike.java
@@ -0,0 +1,16 @@
+package com.org;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Bike extends Vehicle{
+	
+	private boolean hasgear;
+
+}
