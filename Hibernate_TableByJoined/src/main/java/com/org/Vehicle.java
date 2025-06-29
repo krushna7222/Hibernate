56538a7ede9645569894c9475411c2533b28ac6c
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-06-29 22:19:37 +0530

    Create Vehicle.java

---
+++ Hibernate_TableByJoined/src/main/java/com/org/Vehicle.java
@@ -0,0 +1,27 @@
+package com.org;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Vehicle {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String name;
+
+}
