fdb705b1d5ad5435c750996fed84407302cc3d6e
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-07-19 12:07:14 +0530

    Create DOJ.java

---
+++ EmbeddedDemo/src/main/java/com/org/DOJ.java
@@ -0,0 +1,17 @@
+package com.org;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DOJ {
+	
+	private int date,year;
+	private String month;
+
+}
